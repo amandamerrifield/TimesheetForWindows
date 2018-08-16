d04ab64ccb1ea97b2951ec0ac2c1d6be4036110b
Amanda <amandamerrifield@outlook.com>
2018-08-16 11:47:59 -0700

    Added the database library where Git can find it, inside the repository root folder.  Hope this works.  Git is a pain in the ass!

---
+++ SsOpsDatabaseLibrary/Entity/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SsOpsDatabaseLibrary.Entity
+{
+    public class Employee
+    {
+        public Employee() { }
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string TaxIdNbr { get; set; }
+		public string IsSalaryYn { get; set; }
+		public string MainPhone { get; set; }
+		public string Gender { get; set; }
+		public string HireDate { get; set; }
+		public string TermDate { get; set; }
+
+	}
+}
---
+++ SsOpsDatabaseLibrary/Entity/Timecard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SsOpsDatabaseLibrary.Entity
+{
+    public class Timecard
+    {
+        public Timecard()
+        {
+
+        }
+
+        //public string Year { get; set; }
+        //public string WeekNumber { get; set; }
+        //public Employee Employee { get; set; }
+        //public DataTable TimecardDetail { get; set; }
+       
+    }
+}
