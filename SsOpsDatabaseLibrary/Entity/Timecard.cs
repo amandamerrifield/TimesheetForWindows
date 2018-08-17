48a5ed8e2d9970ca0bdb9281e1dcfc70301b4fa0
Amanda <amandamerrifield@outlook.com>
2018-08-16 18:56:53 -0700

    Added new stuff

--- SsOpsDatabaseLibrary/Entity/Timecard.cs
+++ SsOpsDatabaseLibrary/Entity/Timecard.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace SsOpsDatabaseLibrary.Entity
 {
     public class Timecard
     {
-        public Timecard()
-        {
+        public Timecard() {}
 
-        }
-
-        //public string Year { get; set; }
-        //public string WeekNumber { get; set; }
-        //public Employee Employee { get; set; }
-        //public DataTable TimecardDetail { get; set; }
+        public string Year { get; set; }
+        public string WeekNumber { get; set; }
+        public Employee EmployeeId { get; set; }
+        public DataTable DetailTable { get; set; }
        
     }
 }
--- SsOpsDatabaseLibrary/OpsDataWriter.cs
+++ SsOpsDatabaseLibrary/OpsDataWriter.cs
@@ -3,11 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using SsOpsDatabaseLibrary.Entity;
+
 
 namespace SsOpsDatabaseLibrary
 {
-    public class OpsDataWriter
+    public sealed class OpsDataWriter : IDisposable
     {
-		public OpsDataWriter() { }
-    }
+		// This class is designed for usage pattern as follows:
+		//   using (OpsDataWriter dbWriter = new OpsDataWriter(connectionString) {
+		//      dbLib.WriteSomething;
+		//   }
+
+		public const string TIMESTAMP_FORMAT = "yyyy-MM-dd_HH:mm:ss_fff";
+		public const string SHORTDATE_FORMAT = "yyyy-MM-dd";
+
+		private static string UniqueTimeStamp = String.Empty;
+		private bool _disposed = false;
+		private SqlConnection _dbConn;
+
+		//Constructor
+		public OpsDataWriter(string connectnString) {
+			//Must have a database connection string
+			_dbConn = new SqlConnection();
+			_dbConn.ConnectionString = connectnString;
+			//Open a connection to the DB for the life of this instance
+			_dbConn.Open();
+		}
+
+		// =================================================
+		#region Public Functions that return a newly generated primary key
+
+		public Int32 CreateTimeCard(Timecard tcard)
+		{
+			Int32 retVal = 0;
+			SqlParameter parm;
+
+			using(SqlCommand cmd = new SqlCommand("sp_InsertTimeCard", _dbConn))
+			{
+				cmd.CommandType = CommandType.StoredProcedure;
+
+				parm = new SqlParameter("@Year", SqlDbType.Int);
+				cmd.Parameters.Add(parm);
+				parm.Value = Convert.ToInt16(tcard.Year);
+
+				parm = new SqlParameter("@WeekNbr", SqlDbType.Int);
+				cmd.Parameters.Add(parm);
+				parm.Value = Convert.ToInt16(tcard.Year);
+
+				parm = new SqlParameter("@EmployeeId", SqlDbType.Int);
+				cmd.Parameters.Add(parm);
+				parm.Value = Convert.ToInt16(tcard.Year);
+
+				retVal = (Int32) cmd.ExecuteScalar();
+			}
+
+			return retVal;
+		}
+
+		#endregion
+
+		// =================================================
+		#region Error Handling Support
+
+		public void LogHardErrorMessage(string title, string msg)
+		{
+			//Hard errors are ones that cant be appended to the database errors table
+			string post = "=== " + DateTime.Now.ToString(TIMESTAMP_FORMAT) + " ===" + "\n" + title + "\n" + msg + "\n";
+			string fileSpec = Directory.GetCurrentDirectory() + @"\ErrorLog.txt";
+			// Swallow any errors and work thru
+			try
+			{
+				LogMessage(post, fileSpec);
+			}
+			finally {}
+		}
+
+		public void LogMessage(string message, string fileSpec)
+		{
+			TextWriter tw = new StreamWriter(fileSpec);
+			tw.WriteLine(message);
+			tw.Close();
+		}
+
+		#endregion
+
+		// =================================================
+		#region IDisposable Support
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if(disposing)
+				{
+					if (_dbConn != null) {
+						if (_dbConn.State == ConnectionState.Open) _dbConn.Close();
+						_dbConn.Dispose();
+					}
+				}
+				_dbConn = null;
+			}
+			_disposed = true;
+		}
+		#endregion
+	}
 }
