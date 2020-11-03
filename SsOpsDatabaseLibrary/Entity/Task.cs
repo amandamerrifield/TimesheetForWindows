eb129e639955b0d46e9a54c383fa1753fbdf007f
RumbaGuy <42447319+RumbaGuy@users.noreply.github.com>
2020-11-03 09:24:54 -0800

    Added a base class for Task named UnCategorizedTask.

--- SsOpsDatabaseLibrary/Entity/Task.cs
+++ SsOpsDatabaseLibrary/Entity/Task.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 
 namespace SsOpsDatabaseLibrary.Entity
 {
-    public class Task
-    {
-        public Task() { }
+	public class Task : UnCategorizedTask
+	{
+		public Task() : base() { }
 
 		public int CategoryId { get; set; }
 		public string CategoryName { get; set; }
-        public int TaskId { get; set; }
-        public string TaskName { get; set; }
-        public string BudgetHours { get; set; }
-        public string ActualHours { get; set; }
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
-		public override String ToString()
-		{
+	}
+
+	public class UnCategorizedTask
+	{
+		public UnCategorizedTask() { }
+
+		public int TaskId { get; set; }
+		public string TaskName { get; set; }
+		public string BudgetHours { get; set; }
+		public string ActualHours { get; set; }
+		public string StartDate { get; set; }
+		public string EndDate { get; set; }
+		public override String ToString() {
 			return TaskName;
 		}
 	}
